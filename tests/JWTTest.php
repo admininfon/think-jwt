afb2fc5d382b0ce923157e9d07245c81113cf444
kangshutian <kangshutian@qimai.cn>
2022-03-27 00:05:56 +0800

    test

---
+++ tests/JWTTest.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of KangST.
+ *
+ * (c) Kang Shutian <kst157521@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Kangst\JWTAuth\Tests;
+
+class JWTTest extends \PHPUnit\Framework\TestCase
+{
+
+}
